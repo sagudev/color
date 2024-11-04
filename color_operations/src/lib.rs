dbeb5fbfeb3161ab257d3f9d67508be729fad5df
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2024-11-04 17:01:28 +0000

    Import the full Linebender lint set (#1)
    This brings us up-to-date with
    https://linebender.org/wiki/canonical-lints/

--- color_operations/src/lib.rs
+++ color_operations/src/lib.rs
@@ -2,7 +2,12 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
 #![cfg_attr(all(not(feature = "std"), not(test)), no_std)]
+// LINEBENDER LINT SET - v1
+// See https://linebender.org/wiki/canonical-lints/
+// These lints aren't included in Cargo.toml because they
+// shouldn't apply to examples and tests
 #![warn(unused_crate_dependencies)]
+#![warn(clippy::print_stdout, clippy::print_stderr)]
 
 //! # Color Operations
 
