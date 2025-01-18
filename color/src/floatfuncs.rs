b0b868ecf74df6430f0d0235f8fefdfab8818e6a
Bruce Mitchener <bruce.mitchener@gmail.com>
2025-01-18 17:51:38 +0700

    Update stable Rust and typos (#132)

--- color/src/floatfuncs.rs
+++ color/src/floatfuncs.rs
@@ -1,6 +1,14 @@
 // Copyright 2024 the Color Authors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
+// In Rust 1.84 (https://github.com/rust-lang/rust/pull/131304), `abs` and
+// `copysign` were added to `core`, so we no longer need these forwarded to
+// libm.
+#![cfg_attr(
+    not(feature = "std"),
+    allow(dead_code, reason = "abs and copysign were added to core in 1.84")
+)]
+
 //! Shims for math functions that ordinarily come from std.
 
 /// Defines a trait that chooses between libstd or libm implementations of float methods.
@@ -35,10 +43,12 @@ macro_rules! define_float_funcs {
 }
 
 define_float_funcs! {
+    // This is not needed once the MSRV is 1.84 or later.
     fn abs(self) -> Self => fabsf;
     fn atan2(self, other: Self) -> Self => atan2f;
     fn cbrt(self) -> Self => cbrtf;
     fn ceil(self) -> Self => ceilf;
+    // This is not needed once the MSRV is 1.84 or later.
     fn copysign(self, sign: Self) -> Self => copysignf;
     fn floor(self) -> Self => floorf;
     fn hypot(self, other: Self) -> Self => hypotf;
